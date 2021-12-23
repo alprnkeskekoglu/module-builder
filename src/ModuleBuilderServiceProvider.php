ebde97b2b1bd3542dd6b16195ae4af2618993ed6
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2021-12-23 23:26:09 +0300

    initial commit

---
+++ src/ModuleBuilderServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dawnstar\ModuleBuilder;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleBuilderServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
